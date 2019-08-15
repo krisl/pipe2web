071a826cbeb430b4ec6430c7a208a60c71c16460
Aaron Lipinski <kris.lipinski@gmail.com>
2019-08-16 11:33:03 +1200

    use sse module

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const http = require('http')
+const SSE = require('sse')
 
 const PORT = 3000
+const EVENT_TYPE = undefined
 
-http
+const server = http
   .createServer((req, res) => {
-    console.log(`Request url ${req.url}`)
-    req.on('close', () => {
-      if (!res.finished) {
-        res.end()
-        console.log(`Closing`)
-      }
-    })
-    res.writeHead(200, {
-      "Connection": "keep-alive",
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache"
-    })
-    res.write("\n")
-
-    setInterval(() => {
-      res.write(`data: ${JSON.stringify({info: 'blah', x: 2})}`)
-      res.write("\n\n")
-    }, 3000)
+    console.log('server handles')
   })
   .listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
+    const sse = new SSE(server, {path: '/events'})
+    sse.on('connection', (client) => {
+      console.log('connected!')
+      let id = 1
+      setInterval(() => {
+        client.send(EVENT_TYPE, JSON.stringify({info: 'blah', x: 2}), id++)
+      }, 3000)
+    })
   })
 
 
