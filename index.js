e076b0c6c0ffb967cd3c6d8fe2e8bdbe73d7e4ec
Aaron Lipinski <kris.lipinski@gmail.com>
2019-08-16 12:27:22 +1200

    with readline

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const SSE = require('sse')
+const readline = require('readline')
 
 const PORT = 3000
 const EVENT_TYPE = undefined
 
-const lines = process.stdin.pipe(require('split2')())
+//const lines = process.stdin.pipe(require('split2')())
+const rl = readline.createInterface({input: process.stdin})
 const server = http
   .createServer((req, res) => {
     console.log('server handles')
@@ -15,7 +17,8 @@ const server = http
     sse.on('connection', (client) => {
       console.log('connected!')
       let id = 1
-      lines.on('data', line => {
+      rl.on('line', line => {
+        line = line.toString()
         console.log({line})
         client.send(EVENT_TYPE, line, id++)
       })
