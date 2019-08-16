a27b71f06246fc3e2c100330de70e104f031f39c
Aaron Lipinski <kris.lipinski@gmail.com>
2019-08-16 12:12:42 +1200

    use event-stream

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const SSE = require('sse')
+es = require('event-stream');
 
 const PORT = 3000
 const EVENT_TYPE = undefined
 
-const lines = process.stdin.pipe(require('split2')())
+const lines = process.stdin.pipe(es.split())
+
 const server = http
   .createServer((req, res) => {
     console.log('server handles')
